e9359669bb0f7678664295714054eb3ab7f5ccc1
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-09-16 13:29:03 +0300

    lab complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,60 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = [message.to_dict() for message in Message.query.all()]
 
-@app.route('/messages/<int:id>')
+        return make_response(
+            jsonify(messages),
+            200
+        )
+    
+    elif request.method == "POST":
+        request_body = request.get_json()
+        message = Message(
+            body=request_body["body"],
+            username=request_body["username"]
+        )
+
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(
+            jsonify(message.to_dict()),
+            200
+        )
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == "PATCH":
+        for column, value in request.get_json().items():
+            if value != None:
+                setattr(message, column, value)
+
+        db.session.commit()
+
+        return make_response(
+            jsonify(message.to_dict()),
+            200
+        )
+    
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "msg": "message deleted"
+        }
+
+        return make_response(
+            jsonify(response_body),
+            200
+        )
 
 if __name__ == '__main__':
     app.run(port=5555)
